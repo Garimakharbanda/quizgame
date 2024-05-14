d60216c0b245430f704c09b46c750d36369b63b8
Gauri Chauhan <156052262+ChauhanGauri@users.noreply.github.com>
2024-05-14 15:57:03 +0530

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,52 @@
+function logfunction(){
+    var name=document.getElementById("name").value;
+    var password=document.getElementById("password").value;
+    if (name=="garvit", "gauri","garima" && password=='123456'){
+        alert("You can now choose your topic of Quiz !")
+        window.location.assign("topics.html")
+    }
+    else{
+        alert("Invalid credentials !")
+    }
+    }
+
+function signupfunction(){
+    alert("You can now choose your topic of Quiz !")
+    window.location.assign("topics.html")
+}
+
+
+function startfunction1(){
+    alert("You will get 15 Seconds for every question! Quiz will start as soon as you click on OK")
+    window.location.assign("GK-1.html")
+}
+function startfunction2(){
+    alert("You will get 15 Seconds for every question! Quiz will start as soon as you click on OK")
+    window.location.assign("IT-1.html")
+}
+function startfunction3(){
+    alert("You will get 15 Seconds for every question! Quiz will start as soon as you click on OK")
+    window.location.assign("HISTORY-1.html")
+}
+function startfunction4(){
+    alert("You will get 15 Seconds for every question! Quiz will start as soon as you click on OK")
+    window.location.assign("SPORTS-1.html")
+}
+function startfunction5(){
+    alert("You will get 15 Seconds for every question! Quiz will start as soon as you click on OK")
+    window.location.assign("MUSIC-1.html")
+}
+function startfunction6(){
+    alert("You will get 15 Seconds for every question! Quiz will start as soon as you click on OK")
+    window.location.assign("ENV-1.html")
+}
+function startfunction7(){
+    alert("You will get 15 Seconds for every question! Quiz will start as soon as you click on OK")
+    window.location.assign("LIT-1.html")
+}
+function startfunction8(){
+    alert("You will get 15 Seconds for every question! Quiz will start as soon as you click on OK")
+    window.location.assign("SPACE-1.html")
+}
+
+
